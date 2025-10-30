d96c2209d7e1654b0dbd3ba80aee67c03273c482
Iván Cotelo Vilanova <ivancotelo11@gmail.com>
2025-10-30 19:03:31 +0100

    Solve Exercise 23

--- src/main/java/com/campusdual/classroom/Exercise.java
+++ src/main/java/com/campusdual/classroom/Exercise.java
@@ -1,18 +1,30 @@
 package com.campusdual.classroom;
 
+import java.sql.SQLOutput;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class Exercise {
     public static Queue<String> createQueue() {
+        Queue<String> cola = new LinkedList<>();
 
+        cola.offer("Smith");
+        cola.offer("Montessori");
+        cola.offer("Peralta");
+        cola.offer("House");
+          return cola;
     }
     public static void printAndEmptyQueue(Queue<String> queue) {
+      String remove = queue.poll();
+        System.out.println("Elemento eliminado : " + remove);
+        System.out.println("Queue despois do remove: " + queue );
 
     }
 
     public static void main(String[] args) {
-
+     Queue<String> cola1 = createQueue();
+        System.out.println(cola1);
+     printAndEmptyQueue(cola1);
     }
 
 }
